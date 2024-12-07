79bd2bde73326d8c7c39e7bfb6541cc6e2094d6c
Shilpa Merlin joy <shilpamjoy@gmail.com>
2024-12-07 17:03:56 -0500

    Request data from URL on 'load'

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,29 +7,37 @@
 
 import XCTest
 
+class RemoteFeedLoader {
+    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
+}
+class HTTPClient {
+    static let shared = HTTPClient() //single point of access
+    
+    private init() {}
+    
+    var requestedURL: URL?
+    
+}
 final class RemoteFeedLoaderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        
+        let sut = RemoteFeedLoader()
+        sut.load()
+        
+        //when invoke load RemoteFeedLoader invoke a method in client
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
